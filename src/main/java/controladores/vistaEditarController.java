41ed745f1f849adadaa48b7fa90d91e93fddd301
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-04-06 16:58:08 -0600

    RESULTADOS MOSTRADOS EN LA TABLA

--- src/main/java/controladores/vistaEditarController.java
+++ src/main/java/controladores/vistaEditarController.java
@@ -1,84 +1,49 @@
 package controladores;
 
-import clases.Consultor;
-import com.example.natura_proyect.HelloApplication;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
+import clases.Consultor;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 
 public class vistaEditarController {
 
-    public TableView<Consultor> tablaRegistro;
-
+    @FXML
+    private TableView<Consultor> tablaRegistro;
     @FXML
     private TableColumn<Consultor, String> colId;
-
     @FXML
     private TableColumn<Consultor, String> colNombre;
-
     @FXML
     private TableColumn<Consultor, String> colApellidoPaterno;
-
     @FXML
     private TableColumn<Consultor, String> colApellidoMaterno;
-
     @FXML
     private TableColumn<Consultor, String> colTelefono;
-
     @FXML
     private TableColumn<Consultor, String> colDireccion;
-
     @FXML
     private TableColumn<Consultor, String> colEmail;
+    @FXML
+    private Button btnRegresar;
 
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.colId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        this.colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        this.colApellidoPaterno.setCellValueFactory(new PropertyValueFactory<>("apellidoPaterno"));
-        this.colApellidoMaterno.setCellValueFactory(new PropertyValueFactory<>("apellidoMaterno"));
-        this.colTelefono.setCellValueFactory(new PropertyValueFactory<>("telefono"));
-        this.colDireccion.setCellValueFactory(new PropertyValueFactory<>("direccion"));
-        this.colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+    @FXML
+    public void initialize() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colApellidoPaterno.setCellValueFactory(new PropertyValueFactory<>("apellidoPaterno"));
+        colApellidoMaterno.setCellValueFactory(new PropertyValueFactory<>("apellidoMaterno"));
+        colTelefono.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+        colDireccion.setCellValueFactory(new PropertyValueFactory<>("direccion"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
 
-        tablaRegistro.setItems(obtenerDatos());
-    }
-
-    private ObservableList<Consultor> obtenerDatos() {
-        ObservableList<Consultor> lista = FXCollections.observableArrayList();
         ArrayList<Consultor> listaConsultores = Consultor.getConsultores();
-
-        for (Consultor c: listaConsultores){
-            lista.add(new Consultor(c.getId(), c.getNombre(), c.getApellidoPaterno(), c.getApellidoMaterno(), c.getTelefono(), c.getDireccion(), c.getEmail(), c.getPassword()));
-        }
-        return lista;
+        ObservableList<Consultor> observableList = FXCollections.observableArrayList(listaConsultores);
+        tablaRegistro.setItems(observableList);
     }
-
-
-
-    public void regresar(ActionEvent actionEvent) {
-    try {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vistas/vistaPrincipal.fxml"));
-        Parent mainViewRoot = fxmlLoader.load();
-
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(mainViewRoot, 600, 400));
-        stage.show();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module com.example.natura_proyect {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.example.natura_proyect to javafx.fxml;
-    exports com.example.natura_proyect;
-    exports controladores;
+    opens clases to javafx.base;
     opens controladores to javafx.fxml;
+
+    exports com.example.natura_proyect;
 }
