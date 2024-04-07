1bb353a798261811c8ca84904c6d43e04866f485
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-04-07 14:48:06 -0600

    restrcciones agregadas

--- src/main/java/clases/Pedido.java
+++ src/main/java/clases/Pedido.java
@@ -17,11 +17,18 @@ public class Pedido {
         return instance;
     }
 
+    //ESTO SIRVE PARA AGREGAR PRODUCTOS AL PEDIDO
     public void agregarProducto(Producto producto) {
         this.productos.add(producto);
     }
 
+    //ESTO SIRVE PARA  OBTENER LOS PRODUCTOS DEL PEDIDO
     public ArrayList<Producto> getProductos() {
         return productos;
+    }
+
+    //ESTO SIRVE PARA ELIMINAR PRODUCTOS DEL PEDIDO
+    public void eliminarProducto(Producto producto) {
+        productos.remove(producto);
     }
 }
--- src/main/java/controladores/vistaVerProductosController.java
+++ src/main/java/controladores/vistaVerProductosController.java
@@ -3,6 +3,7 @@ package controladores;
 import clases.Maquillaje;
 import clases.Pedido;
 import clases.Producto;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,6 +12,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -35,11 +38,36 @@ public class vistaVerProductosController {
     @FXML
     private TableColumn<Producto, String> columnaTipo;
 
+    @FXML
+    private TableColumn<Producto, Producto> columnaEliminar;
+
     public void initialize() {
         columnaId.setCellValueFactory(new PropertyValueFactory<>("id"));
         columnaTipo.setCellValueFactory(new PropertyValueFactory<>("tipo"));
         columnaNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         columnaCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+
+        columnaEliminar.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        columnaEliminar.setCellFactory(param -> new TableCell<>() {
+            private final Button deleteButton = new Button("Eliminar");
+
+            @Override
+            protected void updateItem(Producto producto, boolean empty) {
+                super.updateItem(producto, empty);
+
+                if (producto == null) {
+                    setGraphic(null);
+                    return;
+                }
+
+                setGraphic(deleteButton);
+                deleteButton.setOnAction(event -> {
+                    Pedido pedido = Pedido.getInstance();
+                    pedido.eliminarProducto(producto);
+                    tablaProductos.getItems().remove(producto);
+                });
+            }
+        });
 
         Pedido pedido = Pedido.getInstance();
         ObservableList<Producto> productos = FXCollections.observableArrayList(pedido.getProductos());
